8fe7045730ff0630210881a35a819fdcdfe5a31b
370701524 <s221456822@deakin.edu.au>
2023-05-16 00:20:03 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const userModel = require("./models/user");
+const initData = require("./utils");
+
+mongoose
+  .connect("mongodb://192.168.186.130:27017/work", {})
+  .then(() => {
+    console.log("Database Connected");
+    initData();
+  })
+  .catch((err) => console.log(err));
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  const data = await userModel.find();
+  res.send({ msg: "success", data });
+});
+
+app.listen(3000, () => console.log("express is work"));
